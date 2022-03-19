2efe0f4679fe1d67872228030b17ff52df24a798
p-a <peter.augustinak@gmail.com>
2022-03-19 16:41:32 +0100

    Rework from dict to DB storing

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
-from typing import Optional
+import time
 
 from fastapi import FastAPI, Response, status, HTTPException
-from fastapi import Body
 from pydantic import BaseModel
-import uvicorn
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
@@ -15,18 +13,20 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
 
-try:
-    conn = psycopg2.connect(host="localhost", port='5433', database='fastapi',
-                            user='postgres', password='password',
-                            cursor_factory=RealDictCursor)
-    cursor = conn.cursor()
-    print("DB connection successful")
-except Exception as error:
-    print("Connection to DB failed")
-    print(f"Error: {error}")
 
+while True:
+    try:
+        conn = psycopg2.connect(host="localhost", port='5433',
+                                database='fastapi',
+                                user='postgres', password='password',
+                                cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        break
+    except Exception as error:
+        print("Connection to DB failed")
+        print(f"Error: {error}")
+        time.sleep(3)
 
 
 post_id = 1
@@ -35,15 +35,21 @@ my_posts = [
      "published": False, "rating": 1, "id": 0}
 ]
 
-def find_post(id):
+
+def find_post(id_):
     for post in my_posts:
-        if post.get("id") == id:
+        if post.get("id") == id_:
             return post
 
 
-def post_not_exist(id: int):
+def post_not_exist(id_: int):
+    if id_ != -1:
+        message = "No posts found."
+    else:
+        message = f"Post id {id_} not exist!"
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                        detail=f"Post id {id} not exist!")
+                        detail=message)
+
 
 @app.get("/")
 async def root():
@@ -52,7 +58,9 @@ async def root():
 
 @app.get("/posts")
 async def get_posts():
-    return my_posts
+    cursor.execute("""SELECT * FROM posts;""")
+    posts = cursor.fetchall()
+    return posts
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
@@ -60,39 +68,47 @@ def create_post(post: Post):
     posted = create(post)
     return {"data": posted}
 
+
 def create(post):
     global post_id
-    posted = post.dict()
-    posted.update({"id": post_id})
-    post_id += 1
-    my_posts.append(posted)
+    cursor.execute("""INSERT INTO posts (title, content, published) 
+                   VALUES (%s, %s, %s) RETURNING * """,
+                   (post.title, post.content, post.published))
+    conn.commit()
+    posted = cursor.fetchone()
     return posted
 
 
 # must be before post/{id} otherwise 'latest' is taken as {id}
 @app.get("/posts/latest")
 def get_latest_post():
-    post = my_posts[-1]
+    cursor.execute("""SELECT * FROM posts ORDER BY created_at DESC LIMIT 1""")
+    latest_post = cursor.fetchone()
+    if latest_post:
+        return {"data": latest_post}
+    post_not_exist(-1)
+
+
+@app.get("/posts/{id_}")
+def get_post(id_: int):
+    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id_)))
+    post = cursor.fetchone()
     if post:
         return {"data": post}
-    post_not_exist()
+    post_not_exist(id_)
 
 
-@app.get("/posts/{id}")
-def get_post(id: int):
-    post = find_post(id)
-    if post:
-        return {"data": post}
-    post_not_exist(id)
-
-
-@app.put("/posts/{id}")
-def update_post(id: int, new_post: Post):
-    old_post = find_post(id)
-    if old_post:
-        updated_post = update(old_post, new_post)
+@app.put("/posts/{id_}")
+def update_post(id_: int, updated_post: Post):
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, 
+    published = %s WHERE id = %s RETURNING *""",
+                   (updated_post.title, updated_post.content,
+                    updated_post.published, str(id_),))
+    conn.commit()
+    updated_post = cursor.fetchone()
+    if updated_post:
         return {"data": updated_post}
-    post_not_exist(id)
+    post_not_exist(id_)
 
 
 def update(old_post, new_post):
@@ -102,13 +118,15 @@ def update(old_post, new_post):
     return updated_post
 
 
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    post = find_post(id)
-    if post:
-        my_posts.remove(post)
+@app.delete("/posts/{id_}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id_: int):
+    cursor.execute("""DELETE FROM posts WHERE id = %s returning *""",
+                   (str(id_),))
+    conn.commit()
+    deleted_post = cursor.fetchone()
+    if deleted_post:
         return Response(status_code=status.HTTP_204_NO_CONTENT)
-    post_not_exist(id)
+    post_not_exist(id_)
 
 
 # if __name__ == "__main__":
